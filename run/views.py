23404550dce508e32354d388384149057ffb1873
Alec Hannaford <alec322@gmail.com>
2023-02-17 17:33:23 -0800

    second and first view added

--- run/views.py
+++ run/views.py
@@ -4,4 +4,25 @@ from django.views import View
 # Create your views here.
 
 class FirstView(View):
-    pass
+    def get(self, request):
+        return JsonResponse({"hello":"word", "method":request.method})
+    def post(self, request):
+        return JsonResponse({"hello":"word", "method":request.method})
+    def put(self, request):
+        return JsonResponse({"hello":"word", "method":request.method})
+    def delete(self, request):
+        return JsonResponse({"hello":"word", "method":request.method})
+    
+class SecondView(View):
+    def get(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param":param, "query":query})
+    def post(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param":param, "query": query})
+    def put(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param":param, "query": query})
+    def delete(self, request, param):
+        query = request.GET.get("query", "no query")
+        return JsonResponse({"param":param, "query": query})
--- runApp/urls.py
+++ runApp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from run.views import FirstView, SecondView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('first/', FirstView.as_view()),
+    path('second/<param>/', SecondView.as_view()),
+    path('admin/', admin.site.urls), 
 ]
